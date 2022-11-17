7ecd8025bbd3e856ebb0b58452a8aeb9eb09789b
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-11-17 15:24:25 +0100

    rinizio esercizio

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,12 @@
 <template>
-  <NavComponent />
-  <MainComponent />
-  <CardComponent />
-  <CardsComponent />
+  <div>
 
+  </div>
 </template>
 
 <script>
-import CardComponent from './components/CardComponent.vue';
-import CardsComponent from './components/CardsComponent.vue';
-import MainComponent from './components/MainComponent.vue';
-import NavComponent from './components/NavComponent.vue';
-
 export default {
-  components: {
-    NavComponent,
-    MainComponent,
-    CardComponent,
-    CardsComponent
-  }
+
 }
 </script>
 
--- src/store.js
+++ src/store.js
@@ -1,6 +1,12 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    apiURL: 'https://api.themoviedb.org/3',
-
+    baseURL: 'https://api.themoviedb.org/3',
+    endPoint: '/search/movie',
+    params: {
+        api_key: '24f3a4eaf12cd16ff49fb04081d7d6e3',
+        query: '',
+        language: 'it-IT'
+    },
+    movie: []
 })
