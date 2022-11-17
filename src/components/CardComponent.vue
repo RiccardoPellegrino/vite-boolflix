2b40555d429ef917119e3e7da09c416750d788df
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-11-17 17:49:53 +0100

    flag

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,13 +1,16 @@
 <template>
-    <!-- <div class="card" style="width: 18rem;">
+    <div class="card" style="width: 18rem;">
         <img src="..." class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">{{ item.original_title }}</h5>
             <p class="card-text">{{ item.title }}</p>
             <p>{{ item.vote_average }}</p>
-            <p>{{ item.original_language }}</p>
+            <div class="flag" v-if="flag.includes(item.original_language)">
+                <img :src="'/images/' + item.original_language + '.png'" :alt="item.original_language + 'Flag'">
+            </div>
+            <div v-else>{{ item.original_language }}</div>
         </div>
-    </div> -->
+    </div>
 </template>
 
 <script>
@@ -15,14 +18,25 @@
 export default {
     props: {
         item: Object
+    },
+    data() {
+        return {
+            flag: [
+                'en', 'it', 'us', 'de', 'ja', 'es', 'ir', 'fr'
+            ]
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
-img {
-    width: 100%;
-    box-shadow: 0 0 6px 3px rgba(#000, 0.20);
+.flag {
+    width: 30px;
+
+    img {
+        width: 100%;
+        box-shadow: 0 0 6px 3px rgba(#000, 0.20);
+    }
 }
 
 .card {
--- src/components/ListCardComponent.vue
+++ src/components/ListCardComponent.vue
@@ -2,8 +2,8 @@
     <section class="container-fluid">
         <h2>Movies</h2>
         <div class="row">
-            <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-5">
-                <CardComponent />
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-5" v-for="(item, index) in items" :key="item.id">
+                <CardComponent :item="item" />
             </div>
 
         </div>
