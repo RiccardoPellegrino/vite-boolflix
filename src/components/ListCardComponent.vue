97589a83430074fe66137506ef2fff83476a69c5
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-11-19 16:43:57 +0100

    refs + scroll left right

--- src/components/ListCardComponent.vue
+++ src/components/ListCardComponent.vue
@@ -1,7 +1,9 @@
 <template>
     <section class="container-fluid">
+        <i class="fa-solid fa-chevron-left arrow-left" @click="scrollLeft()"></i>
         <h2 class="mt-4">{{ title }}</h2>
-        <div class="row">
+        <i class="fa-solid fa-chevron-right arrow-right" @click="scrollRight()"></i>
+        <div class="row" ref="cards">
             <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-5" v-for="(item, index) in items" :key="item.id">
                 <CardComponent :item="item" />
             </div>
@@ -26,6 +28,17 @@ export default {
     },
     components: {
         CardComponent
+    },
+    methods: {
+        scrollLeft() {
+            const element = this.$refs.cards;
+            console.log(element)
+            element.scrollBy({ left: -1000, behavior: "smooth", });
+        },
+        scrollRight() {
+            const element = this.$refs.cards;
+            element.scrollBy({ left: 1000, behavior: "smooth", });
+        }
     }
 }
 </script>
